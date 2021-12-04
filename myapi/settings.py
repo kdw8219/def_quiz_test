9114ea41ac6fa56603ff9b94fa22af047bad099c
kdw8219 <kdw8219@gmail.com>
2021-12-04 17:39:05 +0900

    secret key changed

--- myapi/settings.py
+++ myapi/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-n0%tbvtk%09mlzz&@wmhm6ypw+eet^e)6w3x^^ra4#6fe*$krq'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-n0%tbvtk%09mlzz&@wmhm6ypw+eet^e)6w3x^^ra4#6fe*$krq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
